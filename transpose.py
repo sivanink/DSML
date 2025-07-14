f61f0398cc50ab8538722c61c635dd5d1a209e36
Sivani NK <sivanink3@gmail.com>
2025-07-14 13:47:49 +0530

    Add files via upload

---
+++ transpose.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6]])
+
+print("Original matrix:\n", matrix)
+
+transposed_matrix = np.transpose(matrix)
+
+print("\nTransposed matrix:\n", transposed_matrix)
