eaf2b0fd5c47e1cfb9dbe1c6f1ef774abf054792
Sivani NK <sivanink3@gmail.com>
2025-07-14 15:26:59 +0530

    Add files via upload

---
+++ det.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print("Enter elements:")
+a=input("Enter element 1:")
+b=input("Enter element 2:")
+c=input("Enter element 3:")
+d=input("Enter element 4:")
+s=np.array([[int (a),int (b)],[int (c),int (d)]])
+print(s)
+det=np.linalg.det(s)
+print(det)
