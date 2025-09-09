3b94e1cf71819328e66c8df0761a61a2279a9715
Sivani NK <sivanink3@gmail.com>
2025-09-09 14:06:54 +0530

    Add files via upload

---
+++ nbcsv.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+
+df = pd.read_csv('csvfile1.csv')  
+
+print(df.head())
+
+X = df[['Height', 'Weight']] 
+y = df['T-Shirt Size']         
+
+
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=1)
+
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+
+
+y_pred = clf.predict(X_test)
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+
+print("Enter Sample Data:")
+a = float(input("Enter Height in cm: "))
+b = float(input("Enter Weight in kg: "))
+
+sample_df = pd.DataFrame([[a, b]], columns=['Height', 'Weight'])
+
+pred = clf.predict(sample_df)
+pred_label = le.inverse_transform(pred)
+print("Predicted T-Shirt Size:", pred_label[0])
+
+
