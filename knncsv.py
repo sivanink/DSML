8583d82911e23ccf029fc74466128418296567a6
Sivani NK <sivanink3@gmail.com>
2025-09-09 14:38:24 +0530

    Update knncsv.py

--- knncsv.py
+++ knncsv.py
@@ -1,25 +1,22 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
 from sklearn import metrics
 
-#
-df = pd.read_csv('csvfile1.csv')  
+
+df = pd.read_csv('csvfile1.csv') 
 
 
 X = df[['Height', 'Weight']]
-y = df['T-Shirt Size']
-
-le = LabelEncoder()
-y_encoded = le.fit_transform(y)
+y = df['T-Shirt Size'] 
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
 
 
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
+
 
 y_pred = knn.predict(X_test)
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
@@ -31,10 +28,15 @@ b = float(input("Enter Weight in kg: "))
 
 
 sample_df = pd.DataFrame([[a, b]], columns=['Height', 'Weight'])
-
 pred = knn.predict(sample_df)
-pred_label = le.inverse_transform(pred)
-print("Predicted T-Shirt Size:", pred_label[0])
+
+print("Predicted T-Shirt Size:", pred[0])
+
+
+
+
+
+
 
 
 
